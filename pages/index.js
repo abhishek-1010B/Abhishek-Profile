e89f6857c730f2cd21fb3f75888c9d4e6cb6b158
abhishek-1010B <abhishek1010.be21@chitkarauniversity.edu>
2022-09-15 14:29:52 +0530

    Check it now

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 import Blog from '../components/Blog'
 
 export default function Home() {
