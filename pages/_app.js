f3203b97a5c8200d1a479cb5ae7c68d94652bec7
abhishek-1010B <abhishek1010.be21@chitkarauniversity.edu>
2022-09-15 14:04:00 +0530

    I had added some new sections in my website

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+// import About from '../components/About'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <NavBar />
+    <Component {...pageProps} />
+    {/* <About/> */}
+    <Footer/>
+  </>
 }
 
 export default MyApp
